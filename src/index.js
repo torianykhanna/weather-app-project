7814f629cb0dcf1e755428e065d6b9fe045386f2
Torianyk <an.mol4anova@gmail.com>
2023-12-19 12:42:10 +0200

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,39 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#value");
   let temperature = Math.round(response.data.temperature.current);
   let cityElemrnt = document.querySelector("#city");
+  let descriptionElemrnt = document.querySelector("#description");
+  let humidityElemrnt = document.querySelector("#humidity");
+  let windSpeedElemrnt = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElemrnt.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElemrnt.innerHTML = response.data.condition.description;
+  humidityElemrnt.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElemrnt.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = temperature;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.get.Hours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
